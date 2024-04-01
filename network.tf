d088a4ff6de9e68ed0223309e9a40be047444b34
sayurud <duishendissanayake@gmail.com>
2024-04-01 23:43:53 +0530

    creating vpc with tf default modules

---
+++ network.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc-name
+  cidr = var.vpc-cidr
+
+  azs             = var.vpc-azs
+  private_subnets = var.vpc-private_subnets
+  public_subnets  = var.vpc-public_subnets
+
+  private_subnet_names = var.pvt-sub-name
+  public_subnet_names = var.pub-sub-name
+  igw_tags = {
+    Name = var.igw-name
+  }
+
+  manage_default_network_acl = false
+  manage_default_security_group = false
+  manage_default_route_table = false
+  
+
+  tags = {
+    creator = var.creator
+  }
+}
--- variables.tf
+++ variables.tf
@@ -12,6 +12,42 @@ variable "secret_key" {
   type = string
 }
 
+variable "creator" {
+  type = string
+}
+
+
 
 ###################### vpc ######################
 
+variable "vpc-name" {
+  type = string
+}
+
+variable "vpc-cidr" {
+  type = string
+}
+
+variable "vpc-azs" {
+  type = list(string)
+}
+
+variable "vpc-private_subnets" {
+  type = list(string)
+}
+
+variable "vpc-public_subnets" {
+  type = list(string)
+}
+
+variable "pvt-sub-name" {
+  type = list(string)
+}
+
+variable "pub-sub-name" {
+  type = list(string)
+}
+
+variable "igw-name" {
+  type = string
+}
